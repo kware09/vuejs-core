73826f9dd4f516c4bd8a1bc7c3fb0c48bb23a670
Kieran Ware <kware@wishbone.ie>
2017-05-15 23:17:58 +0100

    Second commit

--- src/components/HrAdminDash.vue
+++ src/components/HrAdminDash.vue
@@ -1,5 +1,25 @@
 <template>
-	<div>
-		Hr Admin
+	<div class="row">
+		<div class="col-md-6">
+			<div class="widget small-widget">
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="widget medium-widget">
+			</div>
+		</div>
+		
 	</div>
 </template>
+
+<style>
+	.widget{
+		margin-bottom:15px;
+		margin-top:15px;
+		height:390px;
+		background-color: #ecf0f1;
+		opacity: 0.8;
+		border-radius:10px; 
+	}
+	
+</style>
